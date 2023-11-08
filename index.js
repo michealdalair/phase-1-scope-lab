98bb44cb3349b4124e5cde8b620a6ba9e699ad05
michaljones <micheal.jones2010@yahoo.com>
2023-11-07 21:42:35 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+  var customerName= "bob";
+ 
+ 
+  function upperCaseCustomerName(){
+   customerName= customerName.toUpperCase();
+
+  
+
+}
+
+
+
+function setBestCustomer(){
+     bestCustomer = 'not bob'
+
+  }
+
+
+
+  //See the consequences of declaring a variable in global scope, by writing a new function called overwriteBestCustomer() that changes that bestCustomer variable to 'maybe bob'.
+    function overwriteBestCustomer(){
+bestCustomer='maybe bob'
+
+
+    }
+
+
+     const leastFavoriteCustomer= "mike"
+function changeLeastFavoriteCustomer(){
+        leastFavoriteCustomer= "jones"
+
+}
